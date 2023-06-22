ed4a8584c549dbf77fb7141a5a9121e563d84e33
Toru Hisai <toru@torus.jp>
2023-06-22 20:23:06 +0900

    Mark 1...

--- app/Main.hs
+++ app/Main.hs
@@ -138,3 +138,69 @@ gmFinal s = case (getCode s) of
               []        -> True
               otherwise -> False
 
+-- p. 97
+
+step :: GmState -> GmState
+step state = dispatch i (putCode is state)
+             where (i : is) = getCode state
+
+dispatch :: Instruction -> GmState -> GmState
+dispatch (Pushglobal f) = pushglobal f
+dispatch (Pushint n)    = pushint n
+dispatch Mkap           = mkap
+dispatch (Push n)       = push n
+dispatch (Slide n)      = slide n
+dispatch Unwind         = unwind
+
+pushglobal :: Name -> GmState -> GmState
+pushglobal f state
+  = putStack (a : getStack state) state
+  where a = aLookup (getGlobals state) f (error ("Undeclared global " ++ f))
+
+-- p. 98
+
+pushint :: Int -> GmState -> GmState
+pushint n state
+  = putHeap heap' (putStack (a : getStack state) state)
+  where (heap', a) = hAlloc (getHeap state) (NNum n)
+
+mkap :: GmState -> GmState
+mkap state
+  = putHeap heap' (putStack (a : as') state)
+  where (heap', a)      = hAlloc (getHeap state) (NAp a1 a2)
+        (a1 : a2 : as') = getStack state
+
+push :: Int -> GmState -> GmState
+push n state
+  = putStack (a : as) state
+  where as = getStack state
+        a  = getArg (hLookup (getHeap state) (as !! (n + 1)))
+
+getArg :: Node -> Addr
+getArg (NAp a1 a2) = a2
+
+-- p. 99
+
+slide :: Int -> GmState -> GmState
+slide n state
+  = putStack (a : drop n as) state
+  where (a : as) = getStack state
+
+unwind :: GmState -> GmState
+unwind state
+  = newState (hLookup heap a)
+  where
+    (a : as) = getStack state
+    heap     = getHeap state
+    newState (NNum n)    = state
+    newState (NAp a1 a2) = putCode [Unwind] (putStack (a1 : a : as) state)
+    newState (NGlobal n c)
+      | length as < n = error "Unwinding with too few arguments"
+      | otherwise     = putCode c state
+
+-- p. 270 A.1 The heap type
+
+hInitial :: Heap a
+hAlloc   :: Heap a -> a -> (Heap a, Addr)
+hUpdate  :: Heap a -> Addr -> a -> Heap a
+hFree    :: Heap a -> Addr -> Heap a
